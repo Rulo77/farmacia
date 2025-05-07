dfd8fc3b63030bea5db876a17a1b64d4875662dc
Rulo77 <ragomez1234567@gmail.com>
2025-05-07 12:04:02 -0600

    update card component to use product inputs and improve styling consistency

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Product } from '../../models/products';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './card.component.html',
 })
 export class CardComponent {
-  products: Product[] = [];
+  @Input() product!: Product;
 }
